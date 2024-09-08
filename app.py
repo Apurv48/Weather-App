63a5678eb3c5a47a015071a8e5499fdec56ccf15
Apurvkr48 <apurvkr408@gmail.com>
2024-09-08 20:05:42 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from flask import Flask,render_template,request
 import requests
 
@@ -19,36 +17,10 @@ def getData():
     }
 
     response = requests.get(url,params=param)
-    city = data['name']
     data = response.json()
+    city = data['name']
     return f"data : {data} , city : {city}"
 
-if __name__=="__main__":
-=======
-
-from flask import Flask,render_template,request
-import requests
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/weatherapp",methods=['POST','GET'])
-def getData():
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    param = {
-            'q': request.form.get('city'),
-        'appid':request.form.get('appid'),
-        'metrices':request.form.get('units')
-    }
-
-    response = requests.get(url,params=param)
-    data = response.json()
-    city = data['name']
-    return f"data : {data}"
 
 if __name__=="__main__":
->>>>>>> dfa6ff1484cedf0067caa096d45e4db7459ca29a
     app.run(host='0.0.0.0',port=5003)
