8875551a39cb1c1c5ed84ed30c0fefea7d19051e
Apurv Kumar <158182013+Apurv48@users.noreply.github.com>
2024-09-09 11:16:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,12 @@ def home():
 
 def getData():
     url = "https://api.openweathermap.org/data/2.5/weather"
-    appid = c1452656aebf77480c65ea5895de9a9
 
     
     param = {
             'q': request.form.get('city'),
+            'appid' = c1452656aebf77480c65ea5895de9a9,
+
         'metrices':request.form.get('units')
     }
 
